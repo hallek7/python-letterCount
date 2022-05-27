7dd44d4cb072a5d82d569fa5ebee7b1d7c1db2b3
Halle <37225535+hallek7@users.noreply.github.com>
2022-05-26 21:30:03 -0400

    second refactoring
    this ref issue line #1

--- letter_counter.py
+++ letter_counter.py
@@ -1,16 +1,19 @@
 def Inputword():
     return input("Enter a word : ").lower()
 
+def getVowelCount(words):
+    vowelCount = 0
+    for character in words:
+        if character in ['a', 'e', 'i', 'o','u']:
+            vowelCount = vowelCount + 1
+    return vowelCount
+
 def main():
     print("This prog counts number of vowels in inpute phrase")
     inputeword = Inputword()
-
-    totalwords = 0
-    for character in inputeword:
-        if character in ['a', 'e', 'i', 'o','u']:
-            totalwords = totalwords + 1
-
-    print("Total vowels in your phrase {}".format(totalwords)); 
+    
+    totalVowls = getVowelCount(inputeword)
+    print("Total vowels in your phrase {}".format(totalVowls)); 
 
 if __name__ == "__main__":
    main()
